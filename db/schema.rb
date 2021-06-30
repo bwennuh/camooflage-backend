f107d19e1c15d3dc9ebcebaa41903b165dd3ce8f
bwennuh <brennabruening@gmail.com>
2021-06-30 17:50:16 -0400

    Generate table migration + add fake seed data

---
+++ db/schema.rb
@@ -0,0 +1,104 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_06_30_211027) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "board_pins", force: :cascade do |t|
+    t.bigint "board_id", null: false
+    t.bigint "non_dairy_option_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["board_id"], name: "index_board_pins_on_board_id"
+    t.index ["non_dairy_option_id"], name: "index_board_pins_on_non_dairy_option_id"
+  end
+
+  create_table "boards", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
+
+  create_table "brands", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "non_dairy_options", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.text "allergens"
+    t.string "image"
+    t.bigint "category_id", null: false
+    t.bigint "brand_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["brand_id"], name: "index_non_dairy_options_on_brand_id"
+    t.index ["category_id"], name: "index_non_dairy_options_on_category_id"
+  end
+
+  create_table "recipe_pins", force: :cascade do |t|
+    t.bigint "recipe_id", null: false
+    t.bigint "non_dairy_option_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["non_dairy_option_id"], name: "index_recipe_pins_on_non_dairy_option_id"
+    t.index ["recipe_id"], name: "index_recipe_pins_on_recipe_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.text "ingredients"
+    t.text "instructions"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone_number"
+    t.string "address"
+    t.string "username"
+    t.string "password"
+    t.text "bio"
+    t.string "avatar"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "board_pins", "boards"
+  add_foreign_key "board_pins", "non_dairy_options"
+  add_foreign_key "boards", "users"
+  add_foreign_key "non_dairy_options", "brands"
+  add_foreign_key "non_dairy_options", "categories"
+  add_foreign_key "recipe_pins", "non_dairy_options"
+  add_foreign_key "recipe_pins", "recipes"
+  add_foreign_key "recipes", "users"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,95 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Board.destroy_all
+Recipe.destroy_all
+Brand.destroy_all
+Category.destroy_all
+NonDairyOption.destroy_all
+BoardPin.destroy_all
+RecipePin.destroy_all
+
+
+puts "Seeding data 🌱 🌱 🌱"
+
+puts "Creating users 🐮 🐮 🐮"
+
+10.times do
+  User.create(
+    first_name: Faker::Name.first_name, 
+    last_name: Faker::Name.last_name, 
+    email: Faker::Lorem.word + "@gmail.com", 
+    phone_number: Faker::PhoneNumber.cell_phone, 
+    address: Faker::Address.full_address, 
+    username: Faker::Lorem.word, 
+    password: Faker::Lorem.characters(number: 10), 
+    bio: Faker::Lorem.sentence, 
+    avatar: Faker::Avatar.image
+    )
+end
+
+puts "Creating boards 💟 💟 💟"
+20.times do
+  Board.create(
+    name: Faker::Lorem.word, 
+    description: Faker::Lorem.sentence, 
+    user_id: User.all.ids.sample
+    )
+end
+
+puts "Creating recipes 📝 📝 📝"
+20.times do
+  Recipe.create(
+    name: Faker::Lorem.word, 
+    description: Faker::Lorem.sentence, 
+    ingredients: Faker::Food.ingredient, 
+    instructions: Faker::Food.description, 
+    user_id: User.all.ids.sample
+    )
+end
+
+puts "Creating brands 🛒 🛒 🛒"
+50.times do
+  Brand.create(
+    name: Faker::Company.name
+  )
+end
+
+puts "Creating categories 🥛 🧀 🥯"
+10.times do
+  Category.create(
+    name: Faker::Food.ingredient
+  )
+end
+
+puts "Creating non-dairy options 🍦 🍦 🍦"
+50.times do
+  NonDairyOption.create(
+    name: Faker::Food.ingredient,
+    description: Faker::Lorem.sentence, 
+    allergens: Faker::Food.ingredient, 
+    image: Faker::Avatar.image, 
+    category_id: Category.all.ids.sample, 
+    brand_id: Brand.all.ids.sample
+  )
+end
+
+puts "Creating board pins 🧷 🧷 🧷"
+25.times do
+  BoardPin.create(
+    non_dairy_option_id: NonDairyOption.all.ids.sample, 
+    board_id: Board.all.ids.sample
+  )
+end
+
+puts "Creating recipe pins 📌 📌 📌"
+25.times do
+  RecipePin.create(
+    non_dairy_option_id: NonDairyOption.all.ids.sample, 
+    recipe_id: Recipe.all.ids.sample
+  )
+end
+
+puts "Data seeded 🌼 🌼 🌼"
